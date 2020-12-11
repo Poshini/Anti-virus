dae9bb38f00ca9a9bcc91c5d9ad17e0ef9cdc645
Poshini Ganesh <49205289+Poshini@users.noreply.github.com>
2020-12-11 13:16:40 +0530

    Add files via upload

---
+++ customscanfolder.cpp
@@ -0,0 +1,21 @@
+#include "customscanfolder.hpp"
+
+CustomScanFolder::CustomScanFolder(QObject *parent) : QObject(parent)
+{
+
+}
+
+//=====================================================
+//             PUBLIC METHOD DEFINITIONS
+//=====================================================
+void CustomScanFolder::setFolderLocation(QString folderLocation)
+{
+    this->folderLocation = folderLocation;
+}
+
+//=====================================================
+//             PUBLIC SLOT DEFINITIONS
+//=====================================================
+void CustomScanFolder::scan()
+{
+}
