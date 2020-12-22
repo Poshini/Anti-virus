da28becd774d2592409b3b2071cd33bdeb378968
Poshini Ganesh <49205289+Poshini@users.noreply.github.com>
2020-12-22 18:23:00 +0530

    Add files via upload

---
+++ deletefile.hpp
@@ -0,0 +1,14 @@
+#ifndef DELETEFILE_HPP
+#define DELETEFILE_HPP
+
+#include <QString>
+#include <QFile>
+
+class DeleteFile
+{
+public:
+    DeleteFile();
+    DeleteFile(QString fileLocation);
+};
+
+#endif // DELETEFILE_HPP
