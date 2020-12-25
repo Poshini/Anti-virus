332a23f5e58676d75956ce5196131af6fa4f4ef2
Poshini Ganesh <49205289+Poshini@users.noreply.github.com>
2020-12-25 11:45:07 +0530

    Add files via upload

---
+++ drives.cpp
@@ -0,0 +1,19 @@
+#include "drives.hpp"
+#include<QFileInfo>
+#include<QDir>
+
+
+Drives::Drives(QObject *parent) : QObject(parent)
+{
+
+}
+
+
+void Drives::add()
+{
+   QDir driveList;
+   for(QFileInfo drive : driveList.drives())
+   {
+        emit sendDriveLetter(drive.absoluteFilePath());
+   }
+}
